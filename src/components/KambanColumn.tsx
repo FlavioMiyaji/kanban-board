a858dad5295434452f5cd6aeaa72ef6850a76998
flavio.miyaji <flavio.miyaji@shx.com.br>
2021-06-08 19:52:05 -0300

    Adding first page

---
+++ src/components/KambanColumn.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './KambanColumn.css';
+
+const KambanColumn: React.FC = (): JSX.Element => {
+  return (
+    <div className="kamban-column">
+      a
+    </div>
+  );
+}
+
+export default KambanColumn;
